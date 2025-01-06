05d9e167ed7fbd6e3f45572cd1d61c8bbd9a7e48
Ritchie <harshvardhan28122003@gmail.com>
2025-01-07 01:39:10 +0530

    TLE-31 problem 6

---
+++ 1896.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int> a(n);
+        for(int i = 0 ; i<n ; i++){
+            cin>>a[i];
+        }
+        int x = *min_element(a.begin()+1 , a.end());
+        if(a[0] > x){
+            cout<<"NO"<<endl;
+        }
+        else{
+            cout<<"YES"<<endl;
+        }
+    }
+ return 0;
+}
